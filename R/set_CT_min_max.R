44dcab980793d2b272a9306570ff7afec64507f2
jc203 <jordi.camps@kuleuven.be>
2017-10-02 12:29:48 +0200

    Added split_genes, a function that splits data into df with hkg genes and endogenous genes

--- R/set_CT_min_max.R
+++ R/set_CT_min_max.R
@@ -1,13 +1,14 @@
 #' Set minimum and maximum CT value
 #'
 #' Changes out of range CT values to minimum or maximum number
+#' @param df Dataframe with raw CT values and column specifiying housekeeping genes
 #' @param min Minimum CT value
 #' @param max Maximum CT value
 #' @return qpcr dataframe
 #' @export
-set_CT_min_max <- function(min = 10, max = 40) {
-  qpcr$CT[qpcr$CT < min] <- NA
-  qpcr$CT[is.na(qpcr$CT)] <- max
-  qpcr$CT[qpcr$CT > max] <- max
-  assign('qpcr', qpcr, envir=.GlobalEnv)
+set_min_max_CT_endog <- function(min = 10, max = 40) {
+  endog$CT[endog$CT < min] <- NA
+  endog$CT[is.na(endog$CT)] <- max
+  endog$CT[endog$CT > max] <- max
+  assign('endog', endog, envir=.GlobalEnv)
 }
---
+++ R/split_genes.R
@@ -0,0 +1,15 @@
+#' Split genes
+#'
+#' Split data into endogenous genes and housekeeping genes
+#' @param df qpcr df
+#' @return endog Dataframe with only CT values from endogenous genes
+#' @return hkg Dataframe with only CT values from housekeeping genes
+#' @export
+split_genes <- function(df) {
+  hkg <- df %>%
+    filter(HKG == TRUE)
+  assign('hkg', hkg, envir = .GlobalEnv)
+  endog <- df %>%
+    filter(HKG == FALSE)
+  assign('endog', endog, envir = .GlobalEnv)
+}
