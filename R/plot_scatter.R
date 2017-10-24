931c5c47dd65cee3e8533a8ebe235a6ff75a7841
jc203 <jordi.camps@kuleuven.be>
2017-10-24 21:54:49 +0200

    Bug fixed for position dodging

--- R/plot_scatter.R
+++ R/plot_scatter.R
@@ -9,10 +9,11 @@
 #' @param nrow Amount of rows
 #' @return Scatter plot
 #' @export
-plot_scatter <- function(df, x.var, y.var, col.var, ncol = 6, nrow = 4, theme = 6) {
-  ggplot(df, aes_string(x = x.var, y = y.var, col = col.var)) +
-    stat_summary(fun.y = mean, geom = "crossbar", col = "black", size = 1, ymin = 0, ymax = 0, position = position_dodge(0.4)) +
-    geom_dotplot(aes(fill = col.var), binaxis = "y", stackdir = "center", dotsize = 1, position = position_dodge(0.4)) +
+plot_scatter <- function(df, x.var, y.var, col.var, ncol = 6, nrow = 4, theme = 8) {
+  ggplot(df, aes_string(x = x.var, y = y.var, fill = col.var)) +
+    stat_summary(fun.y = mean, geom = "crossbar", col = "black", size = 0.2, ymin = 0, ymax = 0, position = position_dodge(1)) +
+    #geom_crossbar(aes(ymin = -40, ymax = -40), position = position_dodge(0.4)) +
+    geom_dotplot(binaxis = "y", stackdir = "center", dotsize = 1, position = position_dodge(1)) +
     facet_wrap(~Gene, ncol = ncol, nrow = nrow, scales = "free") +
     theme_bw(base_size= theme) +
     theme(panel.border = element_blank(),
@@ -30,15 +31,15 @@ plot_scatter <- function(df, x.var, y.var, col.var, ncol = 6, nrow = 4, theme = 
 #' @param y.var Variable to plot on y-axis
 #' @param col.var Variable to plot colours
 #' @export
-plot_scatter_per_gene <- function(df, gene, x.var, y.var, col.var = FALSE) {
+plot_scatter_per_gene <- function(df, gene, x.var, y.var, col.var = FALSE, theme = 14) {
   if (col.var == FALSE) {
     df %>%
       #group_by_(col.var) %>%
       filter(Gene == gene) %>%
-      ggplot(aes_string(x = x.var, y = y.var, col = col.var)) +
+      ggplot(aes_string(x = x.var, y = y.var)) +
       stat_summary(fun.y = mean, geom = "crossbar", col = "black", size = 1, ymin = 0, ymax = 0) +
       geom_dotplot(aes(fill = col.var), binaxis = "y", stackdir = "center", dotsize = 1.5) +
-      theme_bw(base_size=14) +
+      theme_bw(base_size = theme) +
       theme(panel.border = element_blank(),
             panel.grid.major = element_blank(),
             panel.grid.minor = element_blank(),
@@ -49,10 +50,10 @@ plot_scatter_per_gene <- function(df, gene, x.var, y.var, col.var = FALSE) {
     df %>%
       group_by_(col.var) %>%
       filter(Gene == gene) %>%
-      ggplot(aes_string(x = x.var, y = y.var, col = col.var)) +
-      stat_summary(fun.y = mean, geom = "crossbar", col = "black", size = 1, ymin = 0, ymax = 0, position = position_dodge(0.3)) +
-      geom_dotplot(aes(fill = col.var), binaxis = "y", stackdir = "center", dotsize = 1.5, position = position_dodge(0.3)) +
-      theme_bw(base_size=14) +
+      ggplot(aes_string(x = x.var, y = y.var, fill = col.var)) +
+      stat_summary(fun.y = mean, geom = "crossbar", col = "black", size = 0.5, ymin = 0, ymax = 0, position = position_dodge(1)) +
+      geom_dotplot(binaxis = "y", stackdir = "center", dotsize = 1.5, position = position_dodge(1)) +
+      theme_bw(base_size = theme) +
       theme(panel.border = element_blank(),
             panel.grid.major = element_blank(),
             panel.grid.minor = element_blank(),
