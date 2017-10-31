5978f2d58d44ae149c7eb8f6d79f8b11833e6793
jc203 <jordi.camps@kuleuven.be>
2017-10-31 12:02:13 +0100

    Clean files

--- R/read_qpcr.R
+++ R/read_qpcr.R
@@ -8,7 +8,7 @@ read_qpcr <- function(x, sheet = 3, skip = 35) {
   qpcr_colnames <- c("Well Position", "Sample Name", "Target Name", "CT", "Ct Threshold", "Tm1", "Tm2", "Tm3")
   qpcr <- read_excel(x, sheet = sheet, skip = skip, col_names = TRUE)
   qpcr <- qpcr[, qpcr_colnames]
-  real_colnames <- c("Position_well", "Sample", "Primer", "CT", "CT_threshold", "Tm1", "Tm2", "Tm3")
+  real_colnames <- c("Position_well", "Sample", "Target", "CT", "CT_threshold", "Tm1", "Tm2", "Tm3")
   colnames(qpcr) <- real_colnames
   qpcr$CT <- as.numeric(qpcr$CT)
   print(qpcr)
