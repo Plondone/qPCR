78160669a670d6839f677ecca346c6be94795ea2
jc203 <jordi.camps@kuleuven.be>
2018-06-05 12:58:32 +0200

    Update scatter plot

--- R/plot_scatter.R
+++ R/plot_scatter.R
@@ -2,62 +2,33 @@
 #'
 #' Plots a scatterplot for all genes
 #' @param df DCT df
-#' @param x.var Variable to plot on x-axis
-#' @param y.var Variable to plot on y-axis
-#' @param col.var Variable to plot colours
+#' @param x Variable to plot on x-axis
+#' @param y Variable to plot on y-axis
+#' @param color Variable to plot colours
 #' @param ncol Amount of columns
 #' @param nrow Amount of rows
 #' @return Scatter plot
 #' @export
-plot_scatter <- function(df, x.var, y.var, col.var, ncol = 6, nrow = 4, theme = 8) {
-  ggplot(df, aes_string(x = x.var, y = y.var, fill = col.var)) +
-    stat_summary(fun.y = mean, geom = "crossbar", col = "black", size = 0.2, ymin = 0, ymax = 0, position = position_dodge(1)) +
-    #geom_crossbar(aes(ymin = -40, ymax = -40), position = position_dodge(0.4)) +
-    geom_dotplot(binaxis = "y", stackdir = "center", dotsize = 1, position = position_dodge(1)) +
-    facet_wrap(~Gene, ncol = ncol, nrow = nrow, scales = "free") +
-    theme_bw(base_size= theme) +
-    theme(panel.border = element_blank(),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
+plot_scatter <- function(df, x, y, color, gene = NA, dotsize = 1.5, pointrange_size = 2,  ncol = 6, nrow = 4, theme = 8) {
+  if (is.na(gene) == TRUE) {
+    p <- ggplot(df, aes_string(x = x, y = y)) +
+      geom_dotplot(binaxis = "y", stackdir = "center", dotsize = dotsize, stackratio = 1, position = position_dodge(1)) +
+      stat_summary(aes_string(col = color), fun.data = "mean_sdl", fun.args = list(mult = 1), geom = "pointrange", size = pointrange_size, position = position_dodge(1)) +
+      facet_wrap(~Gene, ncol = ncol, nrow = nrow, scales = "free")
+  } else {
+    p <- df %>%
+      dplyr::filter(Gene == gene) %>%
+      ggplot(aes_string(x = x, y = y)) +
+      geom_dotplot(binaxis = "y", stackdir = "center", dotsize = dotsize, stackratio = 1, position = position_dodge(1)) +
+      stat_summary(aes_string(col = color), fun.data = "mean_sdl", fun.args = list(mult = 1), geom = "pointrange", size = pointrange_size, position = position_dodge(1)) +
+      ggtitle(gene)
+  }
+
+  p +
+    theme_bw(base_size = theme) +
+    theme(panel.grid.minor = element_blank(),
           axis.line = element_line(colour = "black"),
           axis.title.x = element_blank(),
-          axis.text.x = element_text(angle = 45, hjust = 1))
-}
-
-#' Plots a scatterplot per gene
-#' @param df DCT df
-#' @param gene Gene to plot
-#' @param x.var Variable to plot on x-axis
-#' @param y.var Variable to plot on y-axis
-#' @param col.var Variable to plot colours
-#' @export
-plot_scatter_per_gene <- function(df, gene, x.var, y.var, col.var = FALSE, theme = 14) {
-  if (col.var == FALSE) {
-    df %>%
-      #group_by_(col.var) %>%
-      filter(Gene == gene) %>%
-      ggplot(aes_string(x = x.var, y = y.var)) +
-      stat_summary(fun.y = mean, geom = "crossbar", col = "black", size = 1, ymin = 0, ymax = 0) +
-      geom_dotplot(aes(fill = col.var), binaxis = "y", stackdir = "center", dotsize = 1.5) +
-      theme_bw(base_size = theme) +
-      theme(panel.border = element_blank(),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            axis.line = element_line(colour = "black"),
-            axis.title.x = element_blank())
-  }
-  else {
-    df %>%
-      group_by_(col.var) %>%
-      filter(Gene == gene) %>%
-      ggplot(aes_string(x = x.var, y = y.var, fill = col.var)) +
-      stat_summary(fun.y = mean, geom = "crossbar", col = "black", size = 0.5, ymin = 0, ymax = 0, position = position_dodge(1)) +
-      geom_dotplot(binaxis = "y", stackdir = "center", dotsize = 1.5, position = position_dodge(1)) +
-      theme_bw(base_size = theme) +
-      theme(panel.border = element_blank(),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            axis.line = element_line(colour = "black"),
-            axis.title.x = element_blank())
-  }
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          plot.title = element_text(face = "bold.italic", hjust = 0.5))
 }
